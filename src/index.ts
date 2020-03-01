4dbd5ab665b10115206d9e49bc09152181daf92a
esnode85 <esnodejs@gmail.com>
2020-03-01 09:08:30 +0200

    use compose

--- src/index.ts
+++ src/index.ts
@@ -19,6 +19,8 @@ const resolvers = {
 const server = new GraphQLServer({ typeDefs, resolvers });
 createConnection()
   .then(() => {
-    server.start(() => console.log("Server is running on localhost:4000"));
+    server.start({ port: 4000 }, () =>
+      console.log("Server is running on localhost:4000")
+    );
   })
   .catch(error => console.log(error));
